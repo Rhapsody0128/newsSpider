f05a33bec002e4c93b6c02e611c4375550042424
Rhapsody0128 <rhapsody0128@gmail.com>
2021-04-09 18:01:38 +0800

    LineFin

--- newsSpider.py
+++ newsSpider.py
@@ -1,37 +1,19 @@
-import requests
+import urllib.request
 import datetime
-
+import os
 from bs4 import BeautifulSoup
-
-News = [
-	"https://today.line.me/tw/v2/article/yKlaPG?utm_source=lineshare",
-	"https://today.line.me/tw/v2/article/pxYeP7?utm_source=lineshare",
-]
-
-sourceDict = {
-	"today.line.me":"LINE Today",
-}
-
-
-# for index,perNews in enumerate(News,start=1):
-#   for i in range(len(sourceDict)):
-# 		if sourceDict[i] in perNews:
-# 			print("aasd")
-  		
-
-
-
 
 
 # News物件 -------------------------------------------------
 class News():
 	def __init__(self,url):
-		response = requests.get(url)
-		self.soup = BeautifulSoup(response.text, "html.parser")
+		response = urllib.request.urlopen(url)
+		self.soup = BeautifulSoup(response, "html.parser")
 		self.date = self.dateToString(datetime.date.today())
 		self.url = url
 		self.source = self.getSource(url)
-
+		self.title = ''
+		self.article = ''
 	def dateToString(self,date):
 		monthDict = {
 			"01":"Jan.",
@@ -51,30 +33,109 @@ class News():
 		dateArr[1] = monthDict.get(dateArr[1],'None')
 		dateString = f"{dateArr[1]} {dateArr[2]}, {dateArr[0]}"
 		return dateString
-
+	def getData(self):	
+		link = f"link:'{self.url}',\n"
+		title = f"title:'{self.title}',\n"
+		content = f"content:{self.content},\n"
+		source = f"source:'{self.source}',\n"
+		date = f"date:'{self.date}',\n"
+		photos = f"photos:{self.photos}"
+		return link + title + content + source + date + photos
 	def getSource(self,url):
 		sourceArr = url.split('/')
 		source = sourceDict[sourceArr[2]]
 		return source
+	def createHTML(self):
+		top = open("data/top.txt",'r',encoding="utf-8")
+		bottom = open("data/bottom.txt",'r',encoding="utf-8")
+		result = top.read() + self.getData() + bottom.read()
+		path = f'C:\\Users\Asus\Desktop\E-News\{self.date}'
+		if not os.path.isdir(path):
+			os.makedirs(path)
+		html = open (f"{path}\{self.title}.html","w",encoding="utf-8")
+		html.write(result)
+		html.close()
 
+
+# NewsType爬蟲 --------------------------------
 
 class LineNews(News):
   def __init__(self,url):
 			super().__init__(url)
-			self.title = self.soup.find("h1").text
-			self.article = self.soup.find('article')
+			self.title = self.soup.find("h1",class_="entityTitle").getText().strip()
+			self.content = []
+			self.photos = []
+			article = self.soup.find('article')
+			for index,item in enumerate(article,start=0):
+				if item.find('img'):
+					self.content.append('')
+					src = item.find('img').get("data-src")
+					remark = item.find("figcaption").getText()
+					photo = {
+						"src":src,
+            "index":index,
+            "remark":remark,
+            "style":{'margin':"auto"}
+					}
+					self.photos.append(photo)
+				else :
+					if item.find('a'):
+						continue
+					elif item.find("b"):
+						self.content.append("!!"+item.getText())
+					else :
+						self.content.append(item.getText())
 
 
-a = LineNews("https://today.line.me/tw/v2/article/pxYeP7?utm_source=lineshare")
-print(a.source)
+class undMoneyNews(News):
+  def __init__(self,url):
+			super().__init__(url)
+			self.title = self.soup.find("h2",id="story_art_title").getText().strip()
+			self.content = []
+			self.photos = []
+			article = self.soup.find('div',id="article_body")
+			for index,item in enumerate(article,start=0):
+				if item.find('img') != -1 and item.find('img') != None:
+					self.content.append('')
+					src = item.find('img').get("src")
+					remark = item.find("figcaption").getText()
+					photo = {
+						"src":src,
+            "index":index,
+            "remark":remark,
+            "style":{'margin':"auto"}
+					}
+					self.photos.append(photo)
+				else :
+					if item.find('a'):
+						continue
+					elif item.find("b"):
+						self.content.append("!!"+item.getText())
+					else :
+						self.content.append(item.getText())
 
 
-# def buildNews(title,link,source,article):
-# 	contnet = title.prettify()  #輸出排版後的HTML內容	
+# 執行 --------------------------------------
+sourceDict = {
+  "today.line.me":"LINE Today",
+  "money.udn.com":"經濟日報"
+}
 
-# 	print(contnet)
-# 	html = open("test.html",'w',encoding="utf-8")	
-# 	html.write(contnet)
-# 	html.close()
-z = open("test.txt",'w')
-z.write("asdsadsad")
+def createNews(source,url):
+  news = News
+  if(source == "today.line.me"):
+    news=LineNews(url)
+  elif(source == "money.udn.com"):
+    news=undMoneyNews(url)
+  news.createHTML()
+
+
+def autoCreate():
+	newsUrl = open("news.txt",'r',encoding="utf-8")
+	for news in newsUrl:
+		for source in sourceDict:
+			if(source in news):
+				createNews(source,news.strip())
+
+autoCreate()
+
